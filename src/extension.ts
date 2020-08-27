889320756aa5f020211e1d9d401a198a08bccaf4
Ayush Mishra <ayushm017@gmail.com>
2020-08-27 21:34:09 +0530

    reordered function definitions

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,87 @@
-import * as vscode from "vscode";
 import prettyBytes from "pretty-bytes";
+import vscode from "vscode";
+
 import { getDependenciesWithoutSize, getPackageSize } from "./helper";
 
 let statusBar: vscode.StatusBarItem;
 
-export async function activate(context: vscode.ExtensionContext) {
+export function calculateTotalSize(
+	packageData: PackageData[] | undefined
+): number {
+	if (packageData !== undefined) {
+		return packageData.reduce((a, b) => a + b.size, 0);
+	}
+	return 0;
+}
+
+export async function update(context: vscode.ExtensionContext): Promise<void> {
+	const cachedDependencies: PackageData[] = context.workspaceState.get(
+		"dependencies"
+	) || [{ name: "", version: "", size: 0 }];
+	const freshDependencies = await getDependenciesWithoutSize();
+
+	const updatedDependencies: PackageData[] = [];
+
+	// add dependencies that were already cached
+	for (const dependency of cachedDependencies) {
+		const foundDependency = freshDependencies.find(
+			(pkg) =>
+				pkg.name === dependency.name &&
+				pkg.version === dependency.version
+		);
+		if (foundDependency !== undefined) {
+			updatedDependencies.push({
+				...foundDependency,
+				size: dependency.size
+			});
+		}
+	}
+
+	// Add new dependencies, if any
+	for (const dependency of freshDependencies) {
+		if (
+			!updatedDependencies.some(
+				(pkg) =>
+					pkg.name === dependency.name &&
+					pkg.version === dependency.version
+			)
+		) {
+			updatedDependencies.push({
+				...dependency,
+				size: await getPackageSize(dependency.name, dependency.version)
+			});
+		}
+	}
+
+	// update workspaceState
+	context.workspaceState.update("dependencies", updatedDependencies);
+	context.workspaceState.update(
+		"dependenciesSize",
+		calculateTotalSize(updatedDependencies)
+	);
+}
+
+export function updateStatusBar(context: vscode.ExtensionContext): void {
+	if (!statusBar) {
+		statusBar = vscode.window.createStatusBarItem(
+			vscode.StatusBarAlignment.Left
+		);
+		statusBar.show();
+	}
+	statusBar.show();
+	const bytes = prettyBytes(
+		context.workspaceState.get("dependenciesSize") || 0
+	);
+	statusBar.text = `$(package) ${bytes}`;
+	statusBar.tooltip = `Unpacked size: ${bytes}`;
+}
+
+export async function activate(
+	context: vscode.ExtensionContext
+): Promise<void> {
 	console.log('Congratulations, your extension "size" is now active!');
 
-	let activeEditor: vscode.TextEditor | undefined =
+	const activeEditor: vscode.TextEditor | undefined =
 		vscode.window.activeTextEditor;
 
 	await update(context);
@@ -38,84 +112,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	updateStatusBar(context);
 }
 
-export async function update(context: vscode.ExtensionContext) {
-	const cachedDependencies: PackageData[] = context.workspaceState.get(
-		"dependencies"
-	) || [{ name: "", version: "", size: 0 }];
-	const freshDependencies = await getDependenciesWithoutSize();
-
-	let updatedDependencies: PackageData[] = [];
-
-	// add dependencies that were already cached
-	for (let dependency of cachedDependencies) {
-		const foundDependency = freshDependencies.find(
-			(pkg) =>
-				pkg.name === dependency.name &&
-				pkg.version === dependency.version
-		);
-		if (foundDependency !== undefined) {
-			updatedDependencies.push({
-				...foundDependency,
-				size: dependency.size
-			});
-		}
-	}
-
-	// Add new dependencies, if any
-	for (let dependency of freshDependencies) {
-		if (
-			!updatedDependencies.some(
-				(pkg) =>
-					pkg.name === dependency.name &&
-					pkg.version === dependency.version
-			)
-		) {
-			updatedDependencies.push({
-				...dependency,
-				size: await getPackageSize(dependency.name, dependency.version)
-			});
-		}
-	}
-
-	// update workspaceState
-	context.workspaceState.update("dependencies", updatedDependencies);
-	context.workspaceState.update(
-		"dependenciesSize",
-		calculateTotalSize(updatedDependencies)
-	);
-}
-
-export function calculateTotalSize(packageData: PackageData[] | undefined) {
-	if (packageData !== undefined) {
-		let sum = 0;
-		packageData.forEach((data) => {
-			if (data.size) {
-				sum += data.size;
-			} else {
-				return 0;
-			}
-		});
-
-		return sum;
-	}
-}
-
-export function updateStatusBar(context: vscode.ExtensionContext) {
-	if (!statusBar) {
-		statusBar = vscode.window.createStatusBarItem(
-			vscode.StatusBarAlignment.Left
-		);
-		statusBar.show();
-	}
-	statusBar.show();
-	const bytes = prettyBytes(
-		context.workspaceState.get("dependenciesSize") || 0
-	);
-	statusBar.text = `$(package) ${bytes}`;
-	statusBar.tooltip = `Unpacked size: ${bytes}`;
-}
-
-export function deactivate() {
+export function deactivate(): void {
 	// context.workspaceState.update("dependencies", undefined);
 	// context.workspaceState.update("dependenciesSize", undefined);
 }
